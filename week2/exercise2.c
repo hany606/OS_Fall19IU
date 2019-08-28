f96c66ed5ae7353f08b5a3b8e6a74abe767ff2aa
hany606 <h.hamed.elanwar@gmail.com>
2019-08-28 15:11:40 +0300

    Add some better modifications in week2

--- week2/exercise2.c
+++ week2/exercise2.c
@@ -3,9 +3,12 @@
 
 int main(){
     char *in;
+    printf("Please enter a string:");
     scanf("%s",in);
+    printf("Reversed String: ");
     for(int i = strlen(in)-1; i >= 0; i--){
         printf("%c", in[i]);
     }
+    printf("\n");
     return 0;
 }
--- week2/exercise4.c
+++ week2/exercise4.c
@@ -8,8 +8,13 @@ void swap(int *n1, int *n2){
 
 int main(){
     int n1, n2;
-    scanf("%d %d", &n1, &n2);
+    printf("Enter first number: ");
+    scanf("%d", &n1);
+    printf("\n");
+    printf("Enter second number: ");
+    scanf("%d", &n2);
+    printf("\n");
     swap(&n1, &n2);
-    printf("%d %d\n", n1, n2);
+    printf("First number: %d, Second number: %d\n", n1, n2);
     return 0;
 }
